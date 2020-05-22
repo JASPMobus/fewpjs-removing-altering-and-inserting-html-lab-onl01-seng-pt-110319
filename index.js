f0cfebfcf115cc9b730d30786be39a6319632826
James Mobus <mobusj@live.com>
2020-05-22 18:39:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 document.querySelector('main#main').remove()
 
-let newHeader = document.bondy.appendChild(createElement('h1#victory'))
+let newHeader = document.body.appendChild(createElement('h1#victory'))
